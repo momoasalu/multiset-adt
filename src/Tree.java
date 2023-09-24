fc67fe514c20d00519bf3a4e9151d5a61be9f486
momoasalu <motunrayoasalu@gmail.com>
2023-09-24 17:24:21 -0400

    Declare instance attributes + implement constructor

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class Tree {
+    private Object root;
+    private ArrayList<Object> subtrees;
+
+    public Tree() {
+        this.root = null;
+        this.subtrees = new ArrayList<>();
+    }
+
+    public Tree(Object root) {
+        this.root = root;
+        this.subtrees = new ArrayList<>();
+    }
+
+    public Tree(Object root, ArrayList<Object> subtrees)  {
+        this.root = root;
+        this.subtrees = subtrees;
+    }
 }
