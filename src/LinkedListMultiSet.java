a6a0a99eee07c50a27dc38e9cbf36ed2f67bbe2d
momoasalu <motunrayoasalu@gmail.com>
2023-09-24 20:55:22 -0400

    Write LinkedListMultiSet methods + attributes

--- src/LinkedListMultiSet.java
+++ src/LinkedListMultiSet.java
@@ -8,27 +8,71 @@ public class LinkedListMultiSet implements MultiSet{
             this.next = null;
         }
     }
+
+    private Node front;
+    private int size;
+
+    public LinkedListMultiSet() {
+        this.front = null;
+        this.size = 0;
+    }
+
     public boolean add(Object item) {
-        return false;
+        Node newNode = new Node(item);
+        newNode.next = this.front;
+        this.front = newNode;
+        this.size += 1;
+        return true;
     }
 
     public void remove(Object item) {
+        Node cur = this.front;
+        Node prev = null;
 
+        while (cur != null) {
+            if (cur.item.equals(item)) {
+                this.size -= 1;
+                if (prev != null) {
+                    prev.next = cur.next;
+                } else {
+                    this.front = cur.next;
+                }
+                return;
+            }
+            prev = cur;
+            cur = cur.next;
+        }
     }
 
     public boolean contains(Object item) {
+        Node cur = this.front;
+        while (cur != null) {
+            if (cur.item.equals(item)) {
+                return true;
+            }
+            cur = cur.next;
+        }
         return false;
     }
 
     public boolean isEmpty() {
-        return false;
+        return this.front == null;
     }
 
     public int count(Object item) {
-        return 0;
+        int numSeen = 0;
+        Node cur = this.front;
+
+        while (cur != null) {
+            if (cur.item.equals(item)) {
+                numSeen += 1;
+            }
+            cur = cur.next;
+        }
+        return numSeen;
     }
 
     public int size() {
-        return 0;
+        return this.size;
     }
 }
