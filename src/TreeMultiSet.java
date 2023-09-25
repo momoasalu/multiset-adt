d6428abbde5ee1e0622bf702ac03d0045acae63c
momoasalu <motunrayoasalu@gmail.com>
2023-09-24 20:19:48 -0400

    Write TreeMultiSet methods + attributes

--- src/TreeMultiSet.java
+++ src/TreeMultiSet.java
@@ -1,2 +1,32 @@
-public class TreeMultiSet {
+public class TreeMultiSet implements MultiSet {
+    private Tree tree;
+
+    public TreeMultiSet() {
+        this.tree = new Tree();
+    }
+
+    public boolean add(Object item) {
+        this.tree.insert(item);
+        return true;
+    }
+
+    public void remove(Object item) {
+        this.tree.deleteItem(item);
+    }
+
+    public boolean contains(Object item) {
+        return this.tree.contains(item);
+    }
+
+    public boolean isEmpty() {
+        return this.tree.isEmpty();
+    }
+
+    public int count(Object item) {
+        return this.tree.count(item);
+    }
+
+    public int size() {
+        return this.tree.length();
+    }
 }
